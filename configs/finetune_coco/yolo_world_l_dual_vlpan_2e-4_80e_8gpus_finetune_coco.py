a5eac17258dec487ede695f2895c27d66500ba12
Emilia O'Brien <emilia-may@hotmail.com>
2024-02-28 07:24:05 +0100

    Started prepping for fine tuning
    Signed-off-by: Emilia O'Brien <emilia-may@hotmail.com>

--- configs/finetune_coco/yolo_world_l_dual_vlpan_2e-4_80e_8gpus_finetune_coco.py
+++ configs/finetune_coco/yolo_world_l_dual_vlpan_2e-4_80e_8gpus_finetune_coco.py
@@ -6,9 +6,9 @@ custom_imports = dict(
     allow_failed_imports=False)
 
 # hyper-parameters
-num_classes = 80
-num_training_classes = 80
-max_epochs = 80  # Maximum training epochs
+num_classes = 16
+num_training_classes = 16
+max_epochs = 5  # Maximum training epochs
 close_mosaic_epochs = 10
 save_epoch_intervals = 5
 text_channels = 512
@@ -98,10 +98,10 @@ coco_train_dataset = dict(
     dataset=dict(
         type='YOLOv5CocoDataset',
         data_root='data/coco',
-        ann_file='annotations/instances_train2017.json',
-        data_prefix=dict(img='train2017/'),
+        ann_file='train.json',
+        data_prefix=dict(img='train/images/'),
         filter_cfg=dict(filter_empty_gt=False, min_size=32)),
-    class_text_path='data/texts/coco_class_texts.json',
+    class_text_path='data/texts/isi.json',
     pipeline=train_pipeline)
 
 train_dataloader = dict(
@@ -123,10 +123,10 @@ coco_val_dataset = dict(
     dataset=dict(
         type='YOLOv5CocoDataset',
         data_root='data/coco',
-        ann_file='annotations/instances_val2017.json',
-        data_prefix=dict(img='val2017/'),
+        ann_file='val.json',
+        data_prefix=dict(img='val/images/'),
         filter_cfg=dict(filter_empty_gt=False, min_size=32)),
-    class_text_path='data/texts/coco_class_texts.json',
+    class_text_path='data/texts/isi.json',
     pipeline=test_pipeline)
 val_dataloader = dict(dataset=coco_val_dataset)
 test_dataloader = val_dataloader
@@ -177,5 +177,5 @@ val_evaluator = dict(
     _delete_=True,
     type='mmdet.CocoMetric',
     proposal_nums=(100, 1, 10),
-    ann_file='data/coco/annotations/instances_val2017.json',
+    ann_file='data/coco/val.json',
     metric='bbox')
